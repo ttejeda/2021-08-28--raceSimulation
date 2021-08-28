2eeec34785e5bb1a0fbe4861d96e3617388f91d8
ttejeda <ttejeda@ucol.mx>
2021-08-27 22:03:33 -0500

    added racing method

--- race.js
+++ race.js
@@ -16,6 +16,38 @@ class Race{
         } else {
             a = 2;
         }
-        return a;
+        
+      return a;
+    }
+
+    racing(){
+        let d1 = 0;
+        let d2 = 0;
+        let t = 0;
+
+        do {
+            d1 = this.roll();
+            this._runnerA += d1;
+            d2 = this.roll();
+            this._runnerB += d2;
+            t++;
+        } while (this._runnerA <= 100 && this._runnerB <= 100);
+
+        console.log(`El corredor 1 recorrió ${this._runnerA} unidades.`);
+        console.log(`El corredor 2 recorrió ${this._runnerB} unidades.`);
+        console.log(`Se jugaron ${t} rondas`);
+        
+        if(this._runnerA >= 100 && this._runnerB >= 100){
+            return `¡Empate!`;
+        } if(this._runnerA >= 100 && this._runnerB <= 100){
+            return `Corredor 1, gana.`;
+        } if(this._runnerB >= 100 && this._runnerA <= 100){
+            return `Corredor 2, gana.`;
+        }
     }
 }
+
+let r1 = new Race();
+let r2 = new Race();
+console.log(r1.racing());
+console.log(r2.racing());
