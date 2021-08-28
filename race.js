06b192ae9233299de20b2a8dd8fdd199c20f4b9e
ttejeda <ttejeda@ucol.mx>
2021-08-27 19:49:11 -0500

    added roll method

--- race.js
+++ race.js
@@ -5,4 +5,17 @@ class Race{
         this._runnerB = 0;
         this._track = 100;
     }
+
+    roll(){
+        let dr = Math.ceil(Math.random() * 6);
+        let a = 0;
+        if(dr < 3){
+            a = 1;
+        } else if(dr == 3){
+            a = 3;
+        } else {
+            a = 2;
+        }
+        return a;
+    }
 }
