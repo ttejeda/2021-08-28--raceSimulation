ffea54ec6e222d665ae1931450d97b8ec83e3980
ttejeda <ttejeda@ucol.mx>
2021-12-20 23:58:34 -0600

    Optimized code (implemented classes: Dice and Runner)

--- race.js
+++ race.js
@@ -1,47 +1,42 @@
-class Race{
+class Dice{
 
     constructor(){
-        this._runnerA = 0;
-        this._runnerB = 0;
-        this._track = 100;
+        this.dice = [1, 1, 3, 2, 2, 2];
     }
-
+    
     roll(){
-        let dr = Math.ceil(Math.random() * 6);
-        let a = 0;
-        if(dr < 3){
-            a = 1;
-        } else if(dr == 3){
-            a = 3;
-        } else {
-            a = 2;
-        }
-        
-      return a;
-    }
-
-    racing(){
-        let d1 = 0;
-        let d2 = 0;
-
-        do {
-            d1 = this.roll();
-            this._runnerA += d1;
-            d2 = this.roll();
-            this._runnerB += d2;
-        } while (this._runnerA <= 100 && this._runnerB <= 100);
-        
-        if(this._runnerA >= 100 && this._runnerB >= 100){
-            return `¡Empate!`;
-        } if(this._runnerA >= 100 && this._runnerB <= 100){
-            return `Corredor 1 gana.`;
-        } if(this._runnerB >= 100 && this._runnerA <= 100){
-            return `Corredor 2 gana.`;
-        }
+        return this.dice[Math.floor(Math.random() * 6)];
     }
 }
 
-let r1 = new Race();
-let r2 = new Race();
-console.log(r1.racing());
-console.log(r2.racing());
+class Runner{
+
+    constructor(name){
+        this._name = name;
+        this._position = 0;
+    }
+
+    getPosition(){
+        return this._position;
+    }
+
+    throwDice(dice){
+        this._position += dice.roll();
+    }
+}
+
+let dice = new Dice()
+let rosa = new Runner("Rosa");
+let alejandra = new Runner("Alejandra");
+while(rosa.getPosition() < 100 && alejandra.getPosition() < 100){
+    rosa.throwDice(dice);
+    alejandra.throwDice(dice);
+}
+
+if(rosa.getPosition() >= 100 && alejandra.getPosition() >= 100){
+    console.log(`¡Empate!`);
+} else if(rosa.getPosition() >= 100){
+    console.log(`¡Rosa gana!`);
+} else{
+    console.log(`¡Alejandra gana!`);
+}
